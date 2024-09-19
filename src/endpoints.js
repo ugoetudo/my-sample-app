d790ad7ec1e8f89deef75a295de942ba86d30b04
ugoetudo <uoe12341234@gmail.com>
2024-09-19 18:32:12 -0400

    create user login view and associated modules

---
+++ src/endpoints.js
@@ -0,0 +1,4 @@
+const name = "endpoints";
+const auth_endpoint = "http://localhost:3000/users"
+
+module.exports = {name, auth_endpoint}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+const endpoints = require('./endpoints.js');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const myFirstElement = <h1>Welcome to Class Finder!</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function WelcomeMessage({first_name, last_name}) {
+  return (
+    <h2>{`Welcome ${first_name} ${last_name}, we are loading your classes for today...`}</h2>
+  )
+}
+
+
+function MyForm() {
+  const [name, setVnumber] = useState("");
+  const [uname, setUName] = useState({});
+
+  const submitVNUM = (e) => {
+    const vn = name
+    e.preventDefault()
+    fetch(`${endpoints.auth_endpoint}?vnum=${vn}`)
+      .then((res) => res.json())
+        .then((data) => setUName({...data})) //ellipsis is the unwrap operator similar to ** in python
+  }
+  if (uname.first_name){
+    return (
+      <div>
+        {myFirstElement}
+        <form onSubmit={submitVNUM}>
+          <label>Enter Your V Number:&nbsp;
+
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setVnumber(e.target.value)} />
+          </label>
+          <input type="submit" />
+        </form>
+        <WelcomeMessage 
+          first_name={uname.first_name}
+          last_name={uname.last_name}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {myFirstElement}
+      <form onSubmit={submitVNUM}>
+        <label>Enter Your V Number:&nbsp;
+
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setVnumber(e.target.value)} />
+        </label>
+        <input type="submit" />
+      </form>
+      
+    </div>
+  )
+}
+
+root.render(
+  <MyForm />
+);
